e2e8a029c1182ae497a8e1068f293151a29bb163
laguierre <84541098+laguierre@users.noreply.github.com>
2021-10-26 13:05:06 -0300

    Initial

--- lib/scr/pages/emergy_page.dart
+++ lib/scr/pages/emergy_page.dart
@@ -1,17 +1,117 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:emergy_app/scr/widgets/big_buttom.dart';
 import 'package:emergy_app/scr/widgets/headers.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ItemBoton {
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton(this.icon, this.texto, this.color1, this.color2);
+}
 
 class EmergencyPage extends StatelessWidget {
   const EmergencyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: BtnEmercency(),
-      ),
+    final items = <ItemBoton>[
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+    ];
+
+    List<Widget> itemMap = items
+        .map((item) => FadeInLeft(
+      duration: Duration(milliseconds: 200),
+          child: BtnEmercency(
+                text: item.texto,
+                onPress: () {},
+                icon: item.icon,
+                Color1: item.color1,
+                Color2: item.color2,
+              ),
+        ))
+        .toList();
+
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          margin: EdgeInsets.only(top: 200),
+          child: ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [SizedBox(height: 80.0), ...itemMap],
+          ),
+        ),
+        _Header(),
+      ]),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconHeader(
+            icon: FontAwesomeIcons.plus,
+            title: 'Asistencia Médica',
+            subtitle: 'Haz solocitado'),
+        Positioned(
+            right: 0,
+            top: 45.0,
+            child: RawMaterialButton(
+              onPressed: (){},
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(15.0),
+              child: FaIcon(FontAwesomeIcons.ellipsisV,
+              color: Colors.white),
+            ))
+      ],
+    );
+  }
+}
+
+class _BtnEmercency extends StatelessWidget {
+  const _BtnEmercency({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BtnEmercency(
+      icon: FontAwesomeIcons.carCrash,
+      text: 'Crush Accident',
+      onPress: () {},
     );
   }
 }
--- lib/scr/widgets/big_buttom.dart
+++ lib/scr/widgets/big_buttom.dart
@@ -2,36 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BtnEmercency extends StatelessWidget {
-  const BtnEmercency({Key? key}) : super(key: key);
+  BtnEmercency({
+    Key? key,
+    this.icon = FontAwesomeIcons.circle,
+    required this.text,
+    this.Color1 = const Color(0xFF6989F5),
+    this.Color2 = const Color(0xFF906EF5),
+    required this.onPress,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String text;
+  final Color Color1;
+  final Color Color2;
+  final VoidCallback onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-
-        _BtnBackground(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 140, width: 40),
-            FaIcon(FontAwesomeIcons.carCrash, color: Colors.white, size: 40),
-            SizedBox(width: 20),
-            Expanded(child: Text('Motor Accident',
-              style: TextStyle(color: Colors.white, fontSize: 18),),
-            ),
-            FaIcon(
-                FontAwesomeIcons.chevronRight, color: Colors.white),
-            SizedBox(width: 40),
-          ],
-        )
-      ],
+    return GestureDetector(
+      onTap: onPress,
+      child: Stack(
+        children: [
+          _BtnBackground(
+            Color1: Color1,
+            Color2: Color2,
+            icon: icon,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 140, width: 40),
+              FaIcon(icon, color: Colors.white, size: 40),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: Text(text,
+                      style: const TextStyle(color: Colors.white, fontSize: 18))),
+              const FaIcon(FontAwesomeIcons.chevronRight, color: Colors.white),
+              const SizedBox(width: 40),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
 
 class _BtnBackground extends StatelessWidget {
-  const _BtnBackground({Key? key}) : super(key: key);
+  const _BtnBackground(
+      {Key? key,
+      required this.Color1,
+      required this.Color2,
+      required this.icon})
+      : super(key: key);
+
+  final IconData icon;
+  final Color Color1;
+  final Color Color2;
 
   @override
   Widget build(BuildContext context) {
@@ -47,8 +73,8 @@ class _BtnBackground extends StatelessWidget {
           ],
           borderRadius: BorderRadius.circular(15.0),
           gradient: LinearGradient(colors: [
-            Color(0xFF6989F5),
-            Color(0xFF906EF5),
+            Color1,
+            Color2,
           ])),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(15),
@@ -57,7 +83,7 @@ class _BtnBackground extends StatelessWidget {
             Positioned(
                 right: -20,
                 top: -20,
-                child: FaIcon(FontAwesomeIcons.carCrash,
+                child: FaIcon(icon,
                     size: 150, color: Colors.white.withOpacity(0.2))),
           ],
         ),
